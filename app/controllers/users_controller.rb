27a6c8017df7166ec2492be2ca916752f9c20c1a
dannychen712 <danny.chen712@gmail.com>
2016-05-04 23:08:02 -0700

    login complete user show complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,6 +63,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :attending)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :attending, :phone_number)
     end
 end
